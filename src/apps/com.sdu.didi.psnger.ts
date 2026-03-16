716fb922ab1b18a289827f225059081050254b2f
brett80 <brett80.xue@gmail.com>
2026-03-16 15:27:29 +0800

    Update com.sdu.didi.psnger.ts

--- src/apps/com.sdu.didi.psnger.ts
+++ src/apps/com.sdu.didi.psnger.ts
@@ -3,8 +3,9 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.sdu.didi.psnger',
   name: '滴滴出行',
-  enable:false,
+
   groups: [
+      enable: true,
     {
       key: 1,
       name: '局部广告-打车页面右上角红包广告',
