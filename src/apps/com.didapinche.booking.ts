aec99796c3e1764554bfa03a5f107259ec32f380
brett80 <brett80.xue@gmail.com>
2026-03-19 10:45:15 +0800

    修改格式2

--- src/apps/com.didapinche.booking.ts
+++ src/apps/com.didapinche.booking.ts
@@ -6,38 +6,32 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '弹窗广告',//我的首页
+      name: '弹窗广告',
       rules: [
         {
+          key:0,//我的首页
           fastQuery: true,
           activityIds:
             'com.didapinche.booking.tab.activity.DiDaTabMainActivity',
           matches:
             '@ImageView < FrameLayout <2 FrameLayout < FrameLayout < FrameLayout < FrameLayout < [id="android:id/content"]',
         },
-      ],
-    },
-      {
-        key: 2,
-      name: '弹窗广告',//我的--》钱包
-      rules: [
         {
+          key:1,//我的--》钱包
           fastQuery: true,
           activityIds: 'com.beizi.ad.v2.activity.BeiZiNewInterstitialActivity',
           matches: '[vid="beizi_interstitial_ad_close_iv"]',
         },
-      ],
-    },
-    {
-      key: 3,
-      name: '弹窗广告',
-      rules: [
         {
-          fastQuery: true,//我的--》全部订单、接单记录
+          key:2,//我的--》全部订单、接单记录
+          fastQuery: true,
           activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
           matches: '@ImageView <3 RelativeLayout < RelativeLayout < RelativeLayout < [id="android:id/content"]',
         },
+
+
       ],
+    
     },
   ],
 });
